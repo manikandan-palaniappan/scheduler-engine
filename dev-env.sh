c1f93993d26da71226f8118c01a76d1787a503d1
manikandan_palaniappan# <drop2palaniappan@gmail.com>
2017-02-26 18:28:46 +0530

    1) Database changed from MySQL to Postgress and its associated changes 2) Database connection and other properties moved to configuration file 3) Dev step up file added for development

---
+++ dev-env.sh
@@ -0,0 +1,14 @@
+export SIMULATION_MODE=false
+
+export DB_HOST=localhost
+export DB_PORT=5432
+export DB_NAME=scheduler
+export DB_USER=postgres
+export DB_PASS=
+
+export SERVER_PORT=8090
+
+export DKRON_HOST=localhost
+export DKRON_PORT=8080
+
+export _JAVA_OPTIONS="-Dlogging.level.com.oneops=DEBUG -Dspring.profiles.active=dev"
