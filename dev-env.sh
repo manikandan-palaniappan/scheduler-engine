1f8227115f38c9ebb6b453edf94a263f43b66646
manikandan_palaniappan# <drop2palaniappan@gmail.com>
2017-03-07 00:51:37 +0530

    Db variable name changes in context to application

--- dev-env.sh
+++ dev-env.sh
@@ -1,14 +1,14 @@
 export SIMULATION_MODE=false
 
-export DB_HOST=localhost
-export DB_PORT=5432
-export DB_NAME=scheduler
-export DB_USER=postgres
-export DB_PASS=
+export SCHEDULER_DB_HOST=localhost
+export SCHEDULER_DB_PORT=5432
+export SCHEDULER_DB_NAME=scheduler
+export SCHEDULER_DB_USER=postgres
+export SCHEDULER_DB_PASS=
 
 export SERVER_PORT=8090
 
 export DKRON_HOST=localhost
 export DKRON_PORT=8080
 
-export _JAVA_OPTIONS="-Dlogging.level.com.oneops=DEBUG -Dspring.profiles.active=dev"
+export _JAVA_OPTIONS="-Dlogging.level.com.appranix=DEBUG -Dspring.profiles.active=dev"
